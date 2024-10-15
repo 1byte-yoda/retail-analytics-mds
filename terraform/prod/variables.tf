6a9a1123119c33661995868a87c820b69dce3828
Mark <canedam2016@gmail.com>
2024-10-15 23:00:37 +0800

    Initial Terraform Deployment Job in CICD

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -1,20 +1,25 @@
 variable "env" {
   description = "The Environment Variable in which the Infrastructure will get created"
-  default     = "prod"
+  default     = "dev"
+  type = string
 }
 
 variable "snowflake_account" {
   description = "The Assigned Snowflake Account ID"
+  type = string
 }
 
 variable "snowflake_username" {
   description = "The username for logging in"
+  type = string
 }
 
 variable "snowflake_password" {
   description = "The password for logging in"
+  type = string
 }
 
 variable "snowflake_role" {
   description = "The role to used for infrastructure manipulation"
+  type = string
 }
--- terraform/stage/s3.tf
+++ terraform/stage/s3.tf
@@ -1,3 +1,7 @@
+#tfsec:ignore:aws-s3-enable-bucket-logging
+#tfsec:ignore:aws-s3-enable-versioning
+#tfsec:ignore:aws-s3-encryption-customer-key
+#tfsec:ignore:aws-s3-enable-bucket-encryption
 resource "aws_s3_bucket" "s3_ae_exam_bucket" {
   bucket = "ae-exam-bucket-${var.env}"
 }
@@ -5,10 +9,10 @@ resource "aws_s3_bucket" "s3_ae_exam_bucket" {
 resource "aws_s3_bucket_public_access_block" "s3_ae_exam_bucket" {
   bucket = aws_s3_bucket.s3_ae_exam_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "policy" {

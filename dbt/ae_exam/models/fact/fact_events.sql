a510db80e34501ebdb7c731e2f707d8b775f3d64
Mark <canedam2016@gmail.com>
2024-10-16 16:28:40 +0800

    Fixed SQLFLuff errors

--- dbt/ae_exam/models/fact/fact_events.sql
+++ dbt/ae_exam/models/fact/fact_events.sql
@@ -8,9 +8,9 @@ WITH _fact_events AS (
         transaction_id
         ,purchase_price
         ,product_value
-        ,product_name,
-        {{ proxy_date_field('TRANSACTION_DATE') }},
-        {{ dbt_utils.generate_surrogate_key(['first_name', 'last_name', 'email']) }} AS customer_id
+        ,product_name
+        ,{{ proxy_date_field('TRANSACTION_DATE') }}
+        ,{{ dbt_utils.generate_surrogate_key(['first_name', 'last_name', 'email']) }} AS customer_id
         ,{{ dbt_utils.generate_surrogate_key(['client_country']) }} AS client_info_id
         ,CURRENT_TIMESTAMP() AS insertion_timestamp
     FROM {{ ref("raw_events") }}
@@ -21,5 +21,5 @@ FROM _fact_events
 WHERE
     1 != 0
     {% if is_incremental() %}
-        AND transaction_id > (SELECT MAX(transaction_id) FROM {{ this }})
+        AND transaction_date > (SELECT MAX(t.transaction_id) AS max_id FROM {{ this }} AS t)
     {% endif %}
